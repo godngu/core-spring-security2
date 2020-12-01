f494b0f70aa9c4fc16f9fd230ff00b0ba7701b37
godngu <godngu@sk.com>
2020-12-01 21:48:47 +0900

    modify jpa ddl-auto config: begin 'create'

--- src/main/java/io/security/corespringsecurity2/security/config/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity2/security/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package io.security.corespringsecurity2.security.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -15,12 +17,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
         String password = passwordEncoder().encode("1111");
-
         auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
-        auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER");
-        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER", "USER");
+        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN", "USER", "MANAGER");
     }
 
     @Bean
@@ -40,5 +40,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http
             .formLogin();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+            .ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
